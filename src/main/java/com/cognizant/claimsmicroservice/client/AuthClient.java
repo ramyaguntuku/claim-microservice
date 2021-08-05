8b06c342f699d45f789cf4f334964b9f278c0dc2
ramyaguntuku <43675204+ramyaguntuku@users.noreply.github.com>
2021-08-06 00:14:25 +0530

    Update AuthClient.java

--- src/main/java/com/cognizant/claimsmicroservice/client/AuthClient.java
+++ src/main/java/com/cognizant/claimsmicroservice/client/AuthClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import com.cognizant.claimsmicroservice.model.AuthenticationResponse;
 
-@FeignClient(name = "authenticationFeignClient",url = "http://Pod3ij033authservice-env.eba-e66cgrtc.ap-south-1.elasticbeanstalk.com")
+@FeignClient(name = "authenticationFeignClient",url = "${AUTH_SERVICE:http://Pod3ij033authservice-env.eba-e66cgrtc.ap-south-1.elasticbeanstalk.com}")
 public interface AuthClient {
 
 	@GetMapping("/authorization/validate") 
